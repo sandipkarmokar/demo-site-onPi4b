c907e329ee5b7ee25f21c0099eca8de607b21cc1
sandip karmokar <skarmokar987@gmail.com>
2025-02-16 14:15:10 +0530

    feat: Update homepage layout with new GIF and source link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main class="flex flex-col justify-center items-center gap-y-4">
+  <img src="/tenor.gif" alt="disappearing kid gif">
+  <a class="underline text-blue-700" href="https://tenor.com/view/disappear-gif-10108384">source</a>
+</main>
